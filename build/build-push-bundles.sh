02651af4436675f5c03f17ea266627f416351f99
Justin Kulikauskas <jkulikau@redhat.com>
2025-04-22 19:31:00 -0400

    use docker naming convention

--- build/build-push-bundles.sh
+++ build/build-push-bundles.sh
@@ -18,27 +18,29 @@ do
     operator_name=$(basename ${dirpath})
     echo "* Handling operator ${operator_name} ..."
     
-    build_img="quay.io/stolostron-grc/${operator_name}:latest"
+    build_img="quay.io/stolostron-grc/${operator_name}"
     for arch in amd64 arm64 s390x ppc64le
     do
         echo "::group::Building ${operator_name} for ${arch}"
-        ${CONTAINER_TOOL} build -f Dockerfile -t "${operator_name}-${arch}" \
+        ${CONTAINER_TOOL} build -f Dockerfile -t "${build_img}-${arch}" \
           --platform "linux/${arch}" --build-arg "TARGETARCH=${arch}" \
           --build-arg "HOSTPLATFORM=${host_platform}" .
         echo "::endgroup::"
     done
-    ${CONTAINER_TOOL} manifest create "${build_img}" "${operator_name}-amd64" \
-      "${operator_name}-arm64" "${operator_name}-s390x" "${operator_name}-ppc64le"
+
+    ${CONTAINER_TOOL} manifest rm "${build_img}:latest" || true
+    ${CONTAINER_TOOL} manifest create "${build_img}:latest" "${build_img}-amd64" \
+      "${build_img}-arm64" "${build_img}-s390x" "${build_img}-ppc64le"
 
     if [[ "${CONTAINER_TOOL}" == "podman" ]]; then
-        echo podman manifest push "${build_img}" "${build_img}"
+        echo podman manifest push "${build_img}:latest" "${build_img}:latest"
     else
-        echo docker manifest push "${build_img}"
+        echo docker manifest push "${build_img}:latest"
     fi
     echo "(faked)"
 
     bundle_img="quay.io/stolostron-grc/${operator_name}-bundle:latest"
-    make bundle IMG=$build_img
+    make bundle "IMG=${build_img}:latest"
     ${CONTAINER_TOOL} build -f bundle.Dockerfile -t ${bundle_img}
     echo ${CONTAINER_TOOL} push ${bundle_img}
     echo "(faked)"
