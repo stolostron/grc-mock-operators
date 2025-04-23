8d6678047673cd29673bda2d0e949fca1c3e22e9
Justin Kulikauskas <jkulikau@redhat.com>
2025-04-22 20:42:47 -0400

    push intermediate images

--- build/build-push-bundles.sh
+++ build/build-push-bundles.sh
@@ -18,30 +18,32 @@ do
     operator_name=$(basename ${dirpath})
     echo "* Handling operator ${operator_name} ..."
     
-    build_img="quay.io/stolostron-grc/${operator_name}"
+    build_img="quay.io/stolostron-grc/${operator_name}:latest"
     for arch in amd64 arm64 s390x ppc64le
     do
         echo "::group::Building ${operator_name} for ${arch}"
-        ${CONTAINER_TOOL} build -f Dockerfile -t "${build_img}-${arch}:latest" \
+        ${CONTAINER_TOOL} build -f Dockerfile -t "${build_img}-${arch}" \
           --platform "linux/${arch}" --build-arg "TARGETARCH=${arch}" \
           --build-arg "HOSTPLATFORM=${host_platform}" .
+        ${CONTAINER_TOOL} push "${build_img}-${arch}"
         echo "::endgroup::"
     done
 
-    ${CONTAINER_TOOL} manifest rm "${build_img}:latest" || true
-    ${CONTAINER_TOOL} manifest create "${build_img}:latest" "${build_img}-amd64:latest" \
-      "${build_img}-arm64:latest" "${build_img}-s390x:latest" "${build_img}-ppc64le:latest"
+    echo "::group::Building multiarch image and bundle for ${operator_name}"
+    ${CONTAINER_TOOL} manifest rm "${build_img}" || true
+    ${CONTAINER_TOOL} manifest create "${build_img}" "${build_img}-amd64" \
+      "${build_img}-arm64" "${build_img}-s390x" "${build_img}-ppc64le"
 
     if [[ "${CONTAINER_TOOL}" == "podman" ]]; then
-        echo podman manifest push "${build_img}:latest" "${build_img}:latest"
+        podman manifest push "${build_img}" "${build_img}"
     else
-        echo docker manifest push "${build_img}:latest"
+        docker manifest push "${build_img}"
     fi
-    echo "(faked)"
 
     bundle_img="quay.io/stolostron-grc/${operator_name}-bundle:latest"
     make bundle "IMG=${build_img}:latest"
     ${CONTAINER_TOOL} build -f bundle.Dockerfile -t ${bundle_img}
-    echo ${CONTAINER_TOOL} push ${bundle_img}
-    echo "(faked)"
+    ${CONTAINER_TOOL} push ${bundle_img}
+
+    echo "::endgroup::"
 done
