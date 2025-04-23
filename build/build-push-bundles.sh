021383105311af510fd8363cfd2a0de61b4db864
Justin Kulikauskas <jkulikau@redhat.com>
2025-04-23 16:51:38 -0400

    Fix image build in github actions
    The updated Dockerfiles use go's ability to cross-compile in order to
    speed up the multi-arch builds - no emulation is needed on the build
    system, and the local image cache can be used for all steps before the
    build. However, they require some additional build args, which the
    scripts and Makefiles now provide.
    
    However, these container builds require a newer version of podman than
    is currently available on the GitHub Actions runners, to honor the
    `--platform` flag inside the `FROM` instructions. So, the workflow now
    uses docker there, which required many changes to the multi-arch build
    scripts. The method is, regrettably, much more in the docker style now,
    but it will still work with podman.
    
    For reference, the builds require buildah v1.37.0, which is in podman
    v5.1.0; github actions currently has podman v4.9.3.
    
    Signed-off-by: Justin Kulikauskas <jkulikau@redhat.com>

--- build/build-push-bundles.sh
+++ build/build-push-bundles.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-set -e
+set -ex
+
+CONTAINER_TOOL="${CONTAINER_TOOL:-podman}"
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null 2>&1 && pwd)"
+
+if [[ "${CONTAINER_TOOL}" == "podman" ]]; then
+    host_platform="$(podman info --format '{{.Version.OsArch}}')"
+else
+    host_platform="$(docker info --format '{{.ClientInfo.Os}}/{{.ClientInfo.Arch}}')"
+fi
 
 for dirpath in $(find ${BASE_DIR}/operators -type d -print -mindepth 1 -maxdepth 1)
 do
@@ -10,18 +18,32 @@ do
     operator_name=$(basename ${dirpath})
     echo "* Handling operator ${operator_name} ..."
     
-    podman manifest rm "${operator_name}" || true
-    podman manifest create "${operator_name}"
     build_img="quay.io/stolostron-grc/${operator_name}:latest"
     for arch in amd64 arm64 s390x ppc64le
     do
-        podman build -f Dockerfile --manifest "${operator_name}" \
-          --platform "linux/${arch}" --build-arg "TARGETARCH=${arch}"
+        echo "::group::Building ${operator_name} for ${arch}"
+        ${CONTAINER_TOOL} build -f Dockerfile -t "${build_img}-${arch}" \
+          --platform "linux/${arch}" --build-arg "TARGETARCH=${arch}" \
+          --build-arg "HOSTPLATFORM=${host_platform}" .
+        ${CONTAINER_TOOL} push "${build_img}-${arch}"
+        echo "::endgroup::"
     done
-    podman manifest push "${operator_name}" "${build_img}"
+
+    echo "::group::Building multiarch image and bundle for ${operator_name}"
+    ${CONTAINER_TOOL} manifest rm "${build_img}" || true
+    ${CONTAINER_TOOL} manifest create "${build_img}" "${build_img}-amd64" \
+      "${build_img}-arm64" "${build_img}-s390x" "${build_img}-ppc64le"
+
+    if [[ "${CONTAINER_TOOL}" == "podman" ]]; then
+        podman manifest push "${build_img}" "${build_img}"
+    else
+        docker manifest push "${build_img}"
+    fi
 
     bundle_img="quay.io/stolostron-grc/${operator_name}-bundle:latest"
-    make bundle IMG=$build_img
-    podman build --platform linux/amd64 -f bundle.Dockerfile  -t ${bundle_img}
-    podman push ${bundle_img}
+    make bundle "IMG=${build_img}"
+    ${CONTAINER_TOOL} build -f bundle.Dockerfile -t ${bundle_img} .
+    ${CONTAINER_TOOL} push ${bundle_img}
+
+    echo "::endgroup::"
 done
